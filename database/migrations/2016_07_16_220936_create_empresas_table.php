0e54d92977ebdf64633098b4880d4fccb2fa305e
iisotec <iisotecperu@gmail.com>
2016-07-16 21:04:29 -0500

    Creacion de un modelo

---
+++ database/migrations/2016_07_16_220936_create_empresas_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEmpresasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('empresas', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string()
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('empresas');
+    }
+}
